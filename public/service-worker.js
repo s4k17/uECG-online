059264dd46e88676bf16d115eee29ce3717869f6
Unknown <sav4ukarsen@gmail.com>
2019-06-28 03:11:27 +0300

    Service worker created

---
+++ public/service-worker.js
@@ -0,0 +1,69 @@
+'use strict';
+var currentdate = new Date();
+var datetime = "TIMESTAMP: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+
+const CACHE_VERSION_NUMBER = 'V1.0 ';
+const CACHE_VERSION_TIMESTAMP = datetime;
+const CACHE_NAME = 'static_chache ' + CACHE_VERSION_NUMBER + CACHE_VERSION_TIMESTAMP;
+const FILES_TO_CACHE = [
+  '/index.html',
+  '/index.js',
+  '/canvas_plot.js',
+  '/matrix.js',
+  '/matrix_position.vert.js',
+  '/color_frag_shader.frag.js',
+  '/styles.css',
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [CACHE_NAME, 'runtime'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open('runtime').then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
